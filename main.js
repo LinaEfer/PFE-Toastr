314ec62a91f03b9655253a227d06cb94ad6faf01
LinaEfer <vaskhen@mail.ru>
2017-01-06 10:11:18 +0200

    test of Toastr

---
+++ main.js
@@ -0,0 +1,12 @@
+function ToastMessage(){
+	
+	this.messageForm = document.getElementById('send-toast');
+
+	this.sendToastButton = document.getElementById('send-my-toast');
+	this.sendToastButton.addEventListener('click',this.sendToast.bind(this));
+};
+
+ToastMessage.prototype.sendToast = function(){
+	alert('You can perform some custom action after a toast goes away');
+
+};
